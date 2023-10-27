4b4193a7e4167de673722c5a469d6614c10de59f
athul-python <athulcmathew121@gmail.com>
2023-10-27 12:13:13 +0530

    asgi updated

--- verbvoyage/asgi.py
+++ verbvoyage/asgi.py
@@ -1,13 +1,11 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-
-
 from .routing import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+django.setup()
 django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': (
