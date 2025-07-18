3ee438aad93d75fbc3cd6aed53dd9f1e4f0926ca
athul-python <athulcmathew121@gmail.com>
2025-07-18 20:16:57 +0530

    added new db sql lite

--- verbvoyage/settings.py
+++ verbvoyage/settings.py
@@ -171,16 +171,21 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'verbvoyage.asgi.application'
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
-        'HOST':config('DB_HOST')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD':config('DB_PASSWORD'),
+#         'HOST':config('DB_HOST')
+#     }
+# }
 AUTH_USER_MODEL = 'Accounts.UserAccount'
 
 APPEND_SLASH = False
